7be630429b5b7e3bed7bbf68abc5e981dbad12f4
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-17 15:00:54 +0530

    instant section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Hero from './components/Hero'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Instant from './components/Instant';
 function App() {
   return (
     <>
       <Hero />
+      <Instant/>
     </>
   )
 }
---
+++ src/components/Instant.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Instant = () => {
+    return (
+        <>
+            <div className="d-flex justify-content-center align-items-center pt-90 pb-180">
+                <div className="container max-w-1060">
+                    <h2 className='font-family-first fw-medium fs-45 lh-128 color-dark-grey text-center mb-30'>Instant Team Chats</h2>
+                    <div className="d-flex justify-content-center mb-60">
+                        <p className='font-family-second fw-normal fs-18 lh-162 text-center color-light-grey max-w-566 mb-0'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa.</p>
+                    </div>
+                    <img className='w-100 h-100 max-h-663' src="./src/assets/images/png/instant-img.png" alt="instant-img" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Instant
