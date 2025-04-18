1aa9e11f9692e2fbc9aca20892f5f7c53f7f8cf7
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-18 09:39:29 +0530

    footer done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Hero from './components/Hero'
 import Instant from './components/Instant';
 import Perfect from './components/Perfect';
+import Footer from './components/footer';
 function App() {
   return (
     <>
       <Hero />
       <Instant />
-      <Perfect/>
+      <Perfect />
+      <Footer/>
     </>
   )
 }
